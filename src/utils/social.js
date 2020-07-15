cacc57a226d70e83e3743a544e2b74fed67ec320
MuhammadZohaib07 <66253658+MuhammadZohaib07@users.noreply.github.com>
2020-07-15 21:57:16 +0500

    Initialize project using Create React App

---
+++ src/utils/social.js
@@ -0,0 +1,3 @@
+export const facebookURL = "https://www.facebook.com/Sagittarius.zebi07/";
+export const githubURL = "https://github.com/MuhammadZohaib07";
+export const linkedInURL = "https://www.linkedin.com/in/muhammad-zohaib-b62a35170/";
